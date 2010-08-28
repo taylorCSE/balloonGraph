0907b1252e1939df1ba5fdfef3edacb3ae011ef2
Jon Craton <jncraton@gmail.com>
2010-08-28 12:38:17 -0400

    - removed extra graphing code

--- src/StatusFrame.cpp
+++ src/StatusFrame.cpp
@@ -105,50 +105,9 @@ void StatusFrame::CreateGUIControls() {
     
     deviceInfo = new wxHtmlWindow(mainPanel);
     mainSizer->Add(deviceInfo, 1, wxEXPAND | wxALL);
-    
+
     Update();
-    
-    //wxComboBox *device_dropdown = new wxComboBox(panel, wxID_ANY, wxT("Device"), wxDefaultPosition, wxDefaultSize);
-    //sizer->Add(device_dropdown, 1, wxEXPAND | wxALL);
 
-    /*
-	// Create a mpFXYVector layer
-	mpFXYVector* vectorLayer = new mpFXYVector(_(""));
-	// Create two vectors for x,y and fill them with data
-	std::vector<double> vectorx, vectory;
-	double xcoord;
-	for (unsigned int p = 0; p < 100; p++) {
-		xcoord = (double)(p*1000.0);
-		vectorx.push_back(25-0.001*pow(xcoord/1000, 2));
-		vectory.push_back(xcoord);
-	}
-	vectorLayer->SetData(vectorx, vectory);
-	vectorLayer->SetContinuity(true);
-	wxPen vectorpen(*wxBLUE, 2, wxSOLID);
-	vectorLayer->SetPen(vectorpen);
-	vectorLayer->SetDrawOutsideMargins(false);
-
-	wxFont graphFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-    m_plot = new mpWindow( this, -1, wxPoint(0,0), wxSize(100,100), wxSUNKEN_BORDER );
-    mpScaleX* xaxis = new mpScaleX(wxT("Temperature"), mpALIGN_BOTTOM, true, mpX_NORMAL);
-    mpScaleY* yaxis = new mpScaleY(wxT("Altitude"), mpALIGN_LEFT, true);
-    xaxis->SetFont(graphFont);
-    yaxis->SetFont(graphFont);
-    xaxis->SetDrawOutsideMargins(false);
-    yaxis->SetDrawOutsideMargins(false);
-	xaxis->SetLabelFormat(wxT("%.2f"));
-	yaxis->SetLabelFormat(wxT("%.1f"));
-    m_plot->SetMargins(30, 30, 50, 100);
-    m_plot->AddLayer(     xaxis );
-    m_plot->AddLayer(     yaxis );
-	m_plot->AddLayer(     vectorLayer );
-    //m_plot->AddLayer(     new mpText(wxT("Altitude vs Temperature"), 10, 10) );
-    mpInfoLegend* leg;
-    m_plot->AddLayer( leg = new mpInfoLegend(wxRect(200,20,40,40), wxTRANSPARENT_BRUSH)); //&hatch2));
-    leg->SetVisible(true);
-    
-    m_plot->Fit();
-    */
 }
 
 void StatusFrame::OnClose(wxCloseEvent& event) {
