885c8c53d1339e8ef0364d75955bf1c59492acfb
Jon Craton <jncraton@gmail.com>
2010-09-17 15:39:59 -0400

    - these should have been added long ago

---
+++ src/BaseFrame.cpp
@@ -0,0 +1,64 @@
+/**
+ * \file BaseFrame.cpp
+ * \brief Base frame Class
+ */
+ 
+#include "BaseFrame.h"
+
+BEGIN_EVENT_TABLE(BaseFrame,wxFrame)
+    EVT_MENU(ID_NEWSTATUS, BaseFrame::NewStatusWindow)
+    EVT_MENU(ID_NEWGRAPH, BaseFrame::NewGraphWindow)
+END_EVENT_TABLE()
+
+BaseFrame::BaseFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &position, const wxSize& size, long style)
+: wxFrame(parent, id, title, position, size, style) {
+}
+
+BaseFrame::~BaseFrame() {
+}
+
+void BaseFrame::CreateMenu() {
+    menubar = new wxMenuBar;
+    wxMenu *devices_menu = new wxMenu;
+    wxMenu *view_menu = new wxMenu;
+    wxMenu *window_menu = new wxMenu;
+    
+    vector<string> deviceIds = DB_getAllDevices();
+    
+    menubar->Append(devices_menu, wxT("&Devices"));
+
+    for(unsigned int i = 0; i < deviceIds.size(); i++) {
+        devices_menu->Append(10000+i, deviceIds[i]);
+    }
+
+    menubar->Append(view_menu, wxT("&View"));
+    
+    view_menu->Append(VIEW_BASIC, wxT("Basic"));
+    view_menu->Append(VIEW_ANALOG, wxT("Analog Channels"));
+
+    menubar->Append(window_menu, wxT("&Window"));
+    
+    window_menu->Append(ID_NEWSTATUS, "New Status Window");
+    window_menu->Append(ID_NEWGRAPH, "New Graph Window");    
+    
+    SetMenuBar(menubar);
+}
+
+
+/** 
+    Launches a new status window
+*/
+
+void BaseFrame::NewStatusWindow( wxCommandEvent& event ) {
+    wxFrame* frame = NewStatusFrame();
+    frame->Show();
+}
+
+/** 
+    Launches a new graph window
+*/
+     
+void BaseFrame::NewGraphWindow( wxCommandEvent& event ) {
+    wxFrame* frame = NewGraphFrame();
+    frame->Show();     
+}
---
+++ src/BaseFrame.h
@@ -0,0 +1,54 @@
+/**
+ * \file BaseFrame.h
+ * \brief Main window header file
+ */
+#ifndef __BASEFRAME_h__
+#define __BASEFRAME_h__
+
+#ifdef __BORLANDC__
+#endif
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+    #include <wx/frame.h>
+#else
+    #include <wx/wxprec.h>
+#endif
+
+#include <wx/menu.h>
+
+#include "database.h"
+
+#undef BaseFrame_STYLE
+#define BaseFrame_STYLE wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxMINIMIZE_BOX | wxMAXIMIZE_BOX | wxCLOSE_BOX
+
+class GraphFrame;
+class StatusFrame;
+
+extern wxFrame* NewGraphFrame();
+extern wxFrame* NewStatusFrame();
+
+class BaseFrame : public wxFrame
+{
+    public:
+        BaseFrame(wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("AppName"), const wxPoint& pos = wxDefaultPosition, const wxSize &size = wxSize(360,600), long style = BaseFrame_STYLE);
+        void NewStatusWindow(wxCommandEvent& event);
+        void NewGraphWindow(wxCommandEvent& event);
+        virtual ~BaseFrame();
+        void CreateMenu();
+
+        wxMenuBar *menubar;
+    private:
+        DECLARE_EVENT_TABLE();
+        // Enumeration for GUI controls
+        enum {
+            ////GUI Enum Control ID Start
+            ID_TEXT = 1000,
+            VIEW_BASIC,
+            VIEW_ANALOG,
+            UPDATE_TIMER,
+            ID_NEWSTATUS,
+            ID_NEWGRAPH
+        };
+};
+#endif
