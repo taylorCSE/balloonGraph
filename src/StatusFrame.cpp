10d34fe76ba8fc6e0c9d268ff29c670a1484c18c
Jon Craton <jncraton@gmail.com>
2010-10-04 12:22:33 -0400

    - added separate view for analog channels

--- src/StatusFrame.cpp
+++ src/StatusFrame.cpp
@@ -36,47 +36,62 @@ void StatusFrame::Update() {
     CreateMenu();
 
     map<string, string> gps_info; 
-    gps_info = DB_getMostRecentGPS(flightId);
+    wxString head,info,tail;
     
-    wxString info = wxString::Format(wxT(""
-        "<body bgcolor=black text=white>"
-        "<b>Device: <font color=#33ff33>%s</font></b>\n"
-        "<hr>\n"
-        "<b>Location</b>\n<br />"
-        "Latitude: <font color=#33ff33>%s</font>\n<br />"
-        "Longitude: <font color=#33ff33>%s</font>\n<br />"
-        "Altitude (M): <font color=#33ff33>%s</font>\n<br />"
-        "Altitude (Ft): <font color=#33ff33>%s</font>\n<br />"
-        "Speed (Knots): <font color=#33ff33>%s</font>\n<br />"
-        "Speed (M/S): <font color=#33ff33>%s</font>\n<br />"
-        "Bearing: <font color=#33ff33>%s</font>\n<br />"
-        "Climb: <font color=#33ff33>%s</font>\n<br />"
-        "GPS Status: <font color=#33ff33>%s</font>\n"
-        "\n<hr>"
-        "<b>Status</b>\n<br />"
-        "Battery 1 (V): \n<br />"
-        "Battery 2 (V): \n<br />"
-        "Buss (V): \n<br />"
-        "Signal (%%): \n<br />"
-        "Temperature Int. (c): \n<br />"
-        "Temperature Ext. (c): \n<br />"
-        "Pressure (HPA): \n<br />"
-        "RH (%%): \n"
-        "</body>"
-        ),
-        flightId.c_str(),
-        gps_info["Latitude"].c_str(),
-        gps_info["Longitude"].c_str(),
-        gps_info["Altitude_m"].c_str(),
-        gps_info["Altitude_ft"].c_str(),
-        gps_info["Spd_knots"].c_str(),
-        gps_info["Spd_mps"].c_str(),
-        gps_info["Hdg"].c_str(),
-        gps_info["Rate_mps"].c_str(),
-        gps_info["Status"].c_str()
-        );
+    head = wxString::Format(wxT(""
+            "<body bgcolor=black text=white>"
+            "<b>Flight: <font color=#33ff33>%s</font></b>\n"
+            "<hr>\n"),flightId.c_str());
+
+    tail = wxString::Format(wxT(""
+            "</body>"));
     
-    deviceInfo->SetPage(info);
+    if(view == VIEW_BASIC) {    
+        gps_info = DB_getMostRecentGPS(flightId);
+
+        info = wxString::Format(wxT(""
+            "<b>Location</b>\n<br />"
+            "Latitude: <font color=#33ff33>%s</font>\n<br />"
+            "Longitude: <font color=#33ff33>%s</font>\n<br />"
+            "Altitude (M): <font color=#33ff33>%s</font>\n<br />"
+            "Altitude (Ft): <font color=#33ff33>%s</font>\n<br />"
+            "Speed (Knots): <font color=#33ff33>%s</font>\n<br />"
+            "Speed (M/S): <font color=#33ff33>%s</font>\n<br />"
+            "Bearing: <font color=#33ff33>%s</font>\n<br />"
+            "Climb: <font color=#33ff33>%s</font>\n<br />"
+            "GPS Status: <font color=#33ff33>%s</font>\n"
+            "\n<hr>"
+            "<b>Status</b>\n<br />"
+            "Battery 1 (V): \n<br />"
+            "Battery 2 (V): \n<br />"
+            "Buss (V): \n<br />"
+            "Signal (%%): \n<br />"
+            "Temperature Int. (c): \n<br />"
+            "Temperature Ext. (c): \n<br />"
+            "Pressure (HPA): \n<br />"
+            "RH (%%): \n"
+            ),
+            gps_info["Latitude"].c_str(),
+            gps_info["Longitude"].c_str(),
+            gps_info["Altitude_m"].c_str(),
+            gps_info["Altitude_ft"].c_str(),
+            gps_info["Spd_knots"].c_str(),
+            gps_info["Spd_mps"].c_str(),
+            gps_info["Hdg"].c_str(),
+            gps_info["Rate_mps"].c_str(),
+            gps_info["Status"].c_str()
+            );
+    }
+    if(view == VIEW_ANALOG) {
+        info = wxString("<b>Analog Channels</b>\n<br />");
+
+        for(int i = 1; i <= 18; i++) {
+            info = info + wxString::Format(wxT(""
+                "A%d: <font color=#33ff33>%s</font>\n<br />"
+                ),i,"test");
+        }
+    }    
+    deviceInfo->SetPage(head+info+tail);
 }
 
 /**
