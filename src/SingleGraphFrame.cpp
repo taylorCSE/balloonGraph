8763a277ce7145802d2a19bc6f90165aba58080f
Jon Craton <jncraton@gmail.com>
2010-10-13 17:49:17 -0400

    - added single graph frame

---
+++ src/SingleGraphFrame.cpp
@@ -0,0 +1,127 @@
+/**
+ * \file SingleGraphFrame.cpp
+ * \brief Main Windows Class
+ */
+ 
+#include "SingleGraphFrame.h"
+
+BEGIN_EVENT_TABLE(SingleGraphFrame,BaseFrame)
+END_EVENT_TABLE()
+
+/**
+   Constructor for the Main frame.
+*/
+
+SingleGraphFrame::SingleGraphFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &position, const wxSize& size, long style)
+: BaseFrame(parent, id, title, position, size, style) {
+    flightId = "Please select a device from the menu.";
+    
+    CreateGUIControls();
+    
+    SetTransparent(245);
+}
+
+/** 
+   Destructor for the Main form.
+*/
+
+SingleGraphFrame::~SingleGraphFrame() {
+}
+
+/**
+    Updates the GUI with new database information
+*/
+
+void SingleGraphFrame::Update() {
+    CreateMenu();
+
+    wxString head,info,tail;
+    
+    head = wxString::Format(wxT(""
+            "<body bgcolor=black text=white>"
+            "<b>Flight: <font color=#33ff33>%s</font></b>\n"
+            "<hr>\n"),flightId.c_str());
+
+    tail = wxString::Format(wxT(""
+            "</body>"));
+    
+    if(view == VIEW_BASIC) {    
+        map<string, string> gps_info = DB_getMostRecentGPS(flightId);
+
+        info = wxString::Format(wxT(""
+            "<b>Location</b>\n<br />"
+            "Latitude: <font color=#33ff33>%s</font>\n<br />"
+            "Longitude: <font color=#33ff33>%s</font>\n<br />"
+            "Altitude (M): <font color=#33ff33>%s</font>\n<br />"
+            "Altitude (Ft): <font color=#33ff33>%s</font>\n<br />"
+            "Speed (Knots): <font color=#33ff33>%s</font>\n<br />"
+            "Speed (M/S): <font color=#33ff33>%s</font>\n<br />"
+            "Bearing: <font color=#33ff33>%s</font>\n<br />"
+            "Climb: <font color=#33ff33>%s</font>\n<br />"
+            "GPS Status: <font color=#33ff33>%s</font>\n"
+            "\n<hr>"
+            "<b>Status</b>\n<br />"
+            "Battery 1 (V): \n<br />"
+            "Battery 2 (V): \n<br />"
+            "Buss (V): \n<br />"
+            "Signal (%%): \n<br />"
+            "Temperature Int. (c): \n<br />"
+            "Temperature Ext. (c): \n<br />"
+            "Pressure (HPA): \n<br />"
+            "RH (%%): \n"
+            ),
+            gps_info["Latitude"].c_str(),
+            gps_info["Longitude"].c_str(),
+            gps_info["Altitude_m"].c_str(),
+            gps_info["Altitude_ft"].c_str(),
+            gps_info["Spd_knots"].c_str(),
+            gps_info["Spd_mps"].c_str(),
+            gps_info["Hdg"].c_str(),
+            gps_info["Rate_mps"].c_str(),
+            gps_info["Status"].c_str()
+            );
+    }
+    if(view == VIEW_ANALOG) {
+        vector<string> analog_data = DB_getMostRecentAnalog(flightId);
+        
+        info = wxString("<b>Analog Channels</b>\n<br />");
+
+        if(analog_data.size() < 19) {
+            info = info + wxString("Data not available.");
+        } else {
+            for(int i = 1; i <= 18; i++) {
+                info = info + wxString::Format(wxT(""
+                    "A%d: <font color=#33ff33>%s</font>\n<br />"
+                    ),i,analog_data[i].c_str());
+            }
+        }
+    }    
+    deviceInfo->SetPage(head+info+tail);
+}
+
+/**
+   Creates all of the GUI controls on the main form.
+*/
+    
+void SingleGraphFrame::CreateGUIControls() {
+
+    
+    // Set window properties and title bar
+    SetTitle(wxT("Device Status"));
+    SetIcon(wxNullIcon);
+    
+    mainPanel = new wxPanel(this, wxID_ANY);
+    mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainPanel->SetSizer(mainSizer);
+    
+    deviceInfo = new wxHtmlWindow(mainPanel);
+    mainSizer->Add(deviceInfo, 1, wxEXPAND | wxALL);
+
+    Update();
+
+}
+
+wxFrame* NewSingleGraphFrame() {
+    return (wxFrame*)(new SingleGraphFrame(NULL));
+}
+
---
+++ src/SingleGraphFrame.h
@@ -0,0 +1,68 @@
+/**
+ * \file SingleGraphFrame.h
+ * \brief Main window header file
+ */
+
+#ifndef __SINGLEFRAME_h__
+#define __SINGLEGRAPHFRAME_h__
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+    #include <wx/frame.h>
+#else
+    #include <wx/wxprec.h>
+#endif
+
+
+#include <wx/timer.h>
+#include <wx/menu.h>
+#include <wx/statusbr.h>
+#include <wx/slider.h>
+#include <wx/stattext.h>
+#include <wx/choice.h>
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/progdlg.h>
+#include <wx/dcbuffer.h>
+#include <wx/html/htmlwin.h>
+                           
+#include "GraphFrame.h"
+#include "mathplot.h"
+#include "database.h"
+#include "BaseFrame.h"
+
+#undef SingleGraphFrame_STYLE
+#define SingleGraphFrame_STYLE wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | wxMINIMIZE_BOX | wxMAXIMIZE_BOX | wxCLOSE_BOX
+
+class SingleGraphFrame : public BaseFrame
+{
+    public:
+        SingleGraphFrame(wxWindow *parent, wxWindowID id = 1, const wxString &title = wxT("AppName"), const wxPoint& pos = wxDefaultPosition, const wxSize &size = wxSize(360,600), long style = SingleGraphFrame_STYLE);
+        virtual ~SingleGraphFrame();
+
+    private:
+        // Event handlers
+        DECLARE_EVENT_TABLE();
+
+        // Functions
+        void Update();
+        void CreateGUIControls();
+        
+        // wxWidgets gui objects
+        wxPanel *mainPanel; 
+        wxBoxSizer *mainSizer;
+        wxHtmlWindow *deviceInfo;
+        
+    private:
+        // Enumeration for GUI controls
+        enum {
+            ////GUI Enum Control ID Start
+            ID_TEXT = 2000,
+        };
+};
+
+#endif
