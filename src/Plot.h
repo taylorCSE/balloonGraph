f8c5db1921c5334d1d474b0e0694bf9193d4be1f
Jon Craton <jncraton@gmail.com>
2010-10-14 13:36:31 -0400

    - added file description

--- src/Plot.h
+++ src/Plot.h
@@ -1,3 +1,11 @@
+/**
+ * \file Plot.h
+ * \brief This file contains the definition for the plot class
+ *
+ * This class could just be a struct, so it has not .cpp file associated
+ * with it.
+ */
+
 #ifndef _PLOT_H_
 #define _PLOT_H_
 
