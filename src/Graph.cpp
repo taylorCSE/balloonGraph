96e50a9d6a5f26467bf5043410a272ae222a4211
Jon Craton <jncraton@gmail.com>
2010-10-14 13:26:44 -0400

    - code cleanup

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -12,18 +12,23 @@ END_EVENT_TABLE()
 Graph::Graph() {
 }
 
-Graph::Graph(wxPanel* panel, string name, string table, string flight_id, string col) :
-   mpWindow(panel, -1, wxPoint(0,0), wxSize(1,1), wxSUNKEN_BORDER) {
-    /**
-    *   Constructor for the Main frame.
-    */
+/**
+   Constructor
+*/
     
-	wxFont windowFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+Graph::Graph(wxPanel* panel, string name, string table, 
+                             string flight_id, string col)
+    : mpWindow(panel, -1, wxPoint(0,0), wxSize(1,1), wxSUNKEN_BORDER) {
+
     this->name = name;
     this->db_table = table;
     this->db_col = col;
     this->flightId = flight_id;
     
+    // Set up the font    
+    wxFont windowFont(10, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, 
+                          wxFONTWEIGHT_NORMAL);
+    // Get the data for the graph
     Plot data = GetData();
 
     string y_label = wxT("Altitude");
@@ -37,8 +42,11 @@ Graph::Graph(wxPanel* panel, string name, string table, string flight_id, string
 	vectorLayer->SetPen(vectorpen);
 	vectorLayer->SetDrawOutsideMargins(false);
 
-    mpScaleX* xaxis = new mpScaleX(x_label, mpALIGN_BOTTOM, true, mpX_NORMAL);
-    mpScaleY* yaxis = new mpScaleY(y_label, mpALIGN_LEFT, true);
+	// Set up the axis
+    mpScaleX* xaxis;
+    mpScaleY* yaxis;
+    xaxis = new mpScaleX(x_label, mpALIGN_BOTTOM, true, mpX_NORMAL);
+    yaxis = new mpScaleY(y_label, mpALIGN_LEFT, true);
     xaxis->SetFont(windowFont);
     yaxis->SetFont(windowFont);
     xaxis->SetDrawOutsideMargins(false);
@@ -46,23 +54,33 @@ Graph::Graph(wxPanel* panel, string name, string table, string flight_id, string
 	xaxis->SetLabelFormat(wxT("%.1f"));
 	yaxis->SetLabelFormat(wxT("%.1f"));
     SetMargins(30, 30, 50, 70);
-    AddLayer(     xaxis );
-    AddLayer(     yaxis );
-	AddLayer(     vectorLayer );
-    AddLayer(     new mpText(y_label + wxT(" vs ") + x_label, 60, 5) );
     
+    // Set up the legend
     mpInfoLegend* leg;
-    AddLayer( leg = new mpInfoLegend(wxRect(200,20,40,40), wxTRANSPARENT_BRUSH)); //&hatch2));
+    leg = new mpInfoLegend(wxRect(200,20,40,40), wxTRANSPARENT_BRUSH);
     leg->SetVisible(true);
-    
+
+    // Add all the layers
+    AddLayer(xaxis);
+    AddLayer(yaxis);
+	AddLayer(vectorLayer);
+    AddLayer(new mpText(y_label + wxT(" vs ") + x_label, 60, 5));
+    AddLayer(leg);
+
+    // Lay items out nicely
     Fit();
 }
 
+/**
+    Destructor
+*/
+
 Graph::~Graph() {
-    /** 
-    *   Destructor for the Main form.
-    */
 }
+
+/**
+    Update the graph with new data
+*/
 
 void Graph::Update(string flight_id) {
     if(flight_id != "") flightId = flight_id;
@@ -83,9 +101,19 @@ void Graph::Update(string flight_id) {
 	UpdateAll();
 }
 
+/**
+    Get new data from the database
+*/
+
 Plot Graph::GetData() {
     return DB_getPlotData((char*)db_table.c_str(),(char*)db_col.c_str(),flightId);
 }
+
+/**
+    Open a single frame version of this graph
+    
+    This event handlers fires on a double click.
+*/
 
 void Graph::OpenSingleGraph(wxMouseEvent& event) {
     wxFrame* frame = NewSingleGraphFrame(this);
