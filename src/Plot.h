74c43ecf4da247cfcbce25268f06ec79da76d277
Jon Craton <jncraton@gmail.com>
2010-10-14 13:37:25 -0400

    - more comment details

--- src/Plot.h
+++ src/Plot.h
@@ -2,6 +2,9 @@
  * \file Plot.h
  * \brief This file contains the definition for the plot class
  *
+ * Objects derived from this class hold the the actual data for a plot.
+ * Plots are generally returned by the database and then graphed.
+ *
  * This class could just be a struct, so it has not .cpp file associated
  * with it.
  */
