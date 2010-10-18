45ddb52a8ebaddef01a7856049a2666db61f7ef9
Jon Craton <jncraton@gmail.com>
2010-10-18 11:17:34 -0400

    - swapped files relating to version number

---
+++ src/version.h
@@ -0,0 +1,6 @@
+/* Automatically generated build information */
+#define VERSION "1.10.822"
+#define VERSION_COMMIT "3f5cede1b2521faa34177207b0a98d4e74458e03"
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 10
+#define VERSION_BUILD 822
