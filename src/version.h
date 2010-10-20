e32a913e0db2c8bda3d1e729e228de84850b1fcc
Jon Craton <jncraton@gmail.com>
2010-10-20 11:32:53 -0400

    - added changes for 1.11

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.10.1020"
+#define VERSION "1.11.1020"
 #define VERSION_COMMIT "1bdb6530f69f41e02440f11378890f22c5927755"
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 10
+#define VERSION_MINOR 11
 #define VERSION_BUILD 1020
