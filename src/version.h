329aaf2cdcdb89562388c04834d222c44eb9f272
Jon Craton <jncraton@gmail.com>
2010-12-15 15:39:55 -0500

    - removed version.txt because it is auto generated

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.14.1054"
-#define VERSION_COMMIT "c51d0f1f3288b6030cd05f80b06cd5a8412360be"
+#define VERSION "1.14.1057"
+#define VERSION_COMMIT "8c7e88bb191108b0daf8478f106a85781522ba29"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 14
-#define VERSION_BUILD 1054
+#define VERSION_BUILD 1057
