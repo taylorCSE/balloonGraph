5b4bbb6af5899d97a6918870c34a952dc47dca17
Jon Craton <jncraton@gmail.com>
2010-12-16 12:03:42 -0500

    - incremented version

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.14.1106"
-#define VERSION_COMMIT "a2c08b0bd29b12145220f303ebcb39dee1738ea8"
+#define VERSION "1.14.1107"
+#define VERSION_COMMIT "cb2b91f83e37f331c3eed7382fdf1ed93294503e"
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 14
-#define VERSION_BUILD 1106
+#define VERSION_MINOR 15
+#define VERSION_BUILD 1107
