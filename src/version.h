3d6c6db84ba0f557559c83229271a0e99183280a
Jon Craton <jncraton@gmail.com>
2011-02-05 14:18:17 -0500

    - updated changelog

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.15.1145"
-#define VERSION_COMMIT ""
+#define VERSION "1.15.1147"
+#define VERSION_COMMIT "58d765f3699dd6ed90a039aaf878ff8c863d05da"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 15
-#define VERSION_BUILD 1145
+#define VERSION_BUILD 1147
