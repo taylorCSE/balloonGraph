d9e83a2f9fbf72f4c5f09105cb22bc3640dd5af2
Jon Craton <jncraton@gmail.com>
2011-04-08 18:24:01 -0400

    - updated version number

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.16.1179"
-#define VERSION_COMMIT "931dafc379734252c6f9f509ce8c98a63277cd58"
+#define VERSION "1.17.1180"
+#define VERSION_COMMIT "b993fc9ee91f275c876a9d6684e8da39ff67a2a0"
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 16
-#define VERSION_BUILD 1179
+#define VERSION_MINOR 17
+#define VERSION_BUILD 1180
