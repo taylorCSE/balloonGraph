a73f791c71aaec0824c26bbaecd1c1fd2e30fad2
Jon Craton <jncraton@gmail.com>
2013-12-31 16:47:50 -0500

    fixed missing function reference

--- src/database.cpp
+++ src/database.cpp
@@ -337,6 +337,15 @@ Plot DB_getPlotData(string data_column, string flight_id) {
 }
 
 
+/**
+    Test to see if there is a query ready.
+*/
+  
+bool DB_isQueryReady() {
+    if(!DB_conn) return false;
+    if(!DB_result) return false;
+    return true;
+}
 
 /** 
     Various unit conversions
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 /* Automatically generated build information */
-#define VERSION "1.19.1212"
-#define VERSION_COMMIT "bfee822f6cf72a75ed67231788698348257b5c42"
+#define VERSION "1.19.1213"
+#define VERSION_COMMIT "ec97376d10460e6da015c810b42662fe6eda22b5"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 19
-#define VERSION_BUILD 1212
+#define VERSION_BUILD 1213
